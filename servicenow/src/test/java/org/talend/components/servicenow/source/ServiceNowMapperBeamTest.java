06f71a1174d3782775410b250572d4ce944962e4
Anas KHABALI <akhabali@talend.com>
2018-02-02 14:51:23 +0000

    fix beam test

--- servicenow/src/test/java/org/talend/components/servicenow/source/ServiceNowMapperBeamTest.java
+++ servicenow/src/test/java/org/talend/components/servicenow/source/ServiceNowMapperBeamTest.java
@@ -7,6 +7,8 @@ import static org.talend.components.servicenow.ServiceNow.PASSWORD;
 import static org.talend.components.servicenow.ServiceNow.USER;
 import static org.talend.sdk.component.junit.SimpleFactory.configurationByExample;
 
+import avro.shaded.com.google.common.collect.Iterables;
+
 import java.io.Serializable;
 
 import javax.json.JsonObject;
@@ -17,7 +19,6 @@ import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.transforms.SimpleFunction;
 import org.apache.beam.sdk.values.PCollection;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.talend.components.servicenow.configuration.BasicAuthConfig;
@@ -25,18 +26,19 @@ import org.talend.components.servicenow.configuration.CommonConfig;
 import org.talend.components.servicenow.configuration.TableDataSet;
 import org.talend.sdk.component.junit.RecordAsserts;
 import org.talend.sdk.component.junit.SimpleComponentRule;
+import org.talend.sdk.component.junit.http.junit4.JUnit4HttpApi;
+import org.talend.sdk.component.junit.http.junit4.JUnit4HttpApiPerMethodConfigurator;
 import org.talend.sdk.component.runtime.beam.TalendIO;
 import org.talend.sdk.component.runtime.input.Mapper;
 
-@Ignore
 public class ServiceNowMapperBeamTest implements Serializable {
 
     @ClassRule
     public static final SimpleComponentRule COMPONENT_FACTORY = new SimpleComponentRule(
             "org.talend.components.servicenow");
 
-    //    @ClassRule
-    //    public static final JUnit4HttpApi API = new JUnit4HttpApi().activeSsl();
+    @ClassRule
+    public static final JUnit4HttpApi API = new JUnit4HttpApi().activeSsl();
 
     //    static {
     //        System.setProperty("talend.junit.http.capture", "true");
@@ -44,9 +46,9 @@ public class ServiceNowMapperBeamTest implements Serializable {
 
     @Rule
     public transient final TestPipeline pipeline = TestPipeline.create();
-    //
-    //    @Rule
-    //    public final JUnit4HttpApiPerMethodConfigurator configurator = new JUnit4HttpApiPerMethodConfigurator(API);
+
+    @Rule
+    public final JUnit4HttpApiPerMethodConfigurator configurator = new JUnit4HttpApiPerMethodConfigurator(API);
 
     @Test
     public void getRecords() {
@@ -57,7 +59,7 @@ public class ServiceNowMapperBeamTest implements Serializable {
         final CommonConfig apiConfig = new CommonConfig();
         apiConfig.setTableName(CommonConfig.Tables.incident);
         configuration.setCommonConfig(apiConfig);
-        configuration.setMaxRecords(1);
+        configuration.setMaxRecords(10);
 
         // We create the component mapper instance using the configuration filled above
         final Mapper mapper = COMPONENT_FACTORY.asManager()
@@ -67,7 +69,15 @@ public class ServiceNowMapperBeamTest implements Serializable {
 
         // create a pipeline starting with the mapper
         final PCollection<JsonObject> out = pipeline.apply(TalendIO.read(mapper));
+
         PAssert.that(out)
+                .satisfies(new SimpleFunction<Iterable<JsonObject>, Void>() {
+
+                    @Override public Void apply(final Iterable<JsonObject> input) {
+                        assertEquals(10, Iterables.size(input));
+                        return null;
+                    }
+                })
                 .satisfies(new SimpleFunction<Iterable<JsonObject>, Void>() {
 
                     @Override
